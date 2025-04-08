d402a973f6d4eef45e212d36dcfe5e56c7b3cc07
Alex White <alex.white@hey.com>
2025-04-07 23:23:09 -0400

    Add colors and fix typography plugin

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,9 +2,9 @@
     <div class="text-white">
 
         <header>
-            <h1 class="text-3xl font-bold mb-3">{{ $article->title }}</h1>
+            <h1 class="text-6xl font-bold mb-8">{{ $article->title }}</h1>
         </header>
-        <div class="prose md:prose-xl">
+        <div class="prose md:prose-xl prose-invert">
             {!!   $article->content   !!}
         </div>
     </div>
